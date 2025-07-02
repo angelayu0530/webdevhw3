5278f568cdc4bd5729736912694d022dd2a1ce1b
Angela Yu <angelayu0530@gmail.com>
2025-07-02 18:30:11 -0400

    about page

---
+++ src/About.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+
+
+      <main className="about">
+        <h1 className="about-title">About In-N-Out Burger</h1>
+        <p>
+          In-N-Out Burger is a regional chain of fast food restaurants with locations
+          primarily in California and the Southwest. Founded in 1948, it is known for
+          its simple menu, fresh ingredients, and commitment to quality. It is also
+          extremely affordable compared to other fast food chains and is best known
+          for its Double-Double.
+        </p>
+
+        <div className="about-container">
+          <div className="about-image-block">
+            <img
+              src="/images/first.jpeg"
+              alt="First In-N-Out Restaurant"
+              className="about-image-block img"
+            />
+            <p>This is the first restaurant of In-N-Out.</p>
+          </div>
+
+          <div className="about-image-block">
+            <img
+              src="/images/second.webp"
+              alt="Current Owner of In-N-Out"
+              className="about-image-block img"
+            />
+            <p>This is the current owner of In-N-Out.</p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default About;
+
+
